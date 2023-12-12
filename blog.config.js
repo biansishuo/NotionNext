4db376b95ef0ef71e1f7e82393dff7088e28128f
biansishuo <53433131+biansishuo@users.noreply.github.com>
2023-12-13 00:31:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '卞思硕', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一位实习律师', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://xianhuan.co', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '法律','北京', '刑事', '刑辩', '犯罪', '诈骗', '帮信','赌博', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '法律', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
